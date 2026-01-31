74b3d917142ba5d7c3b9507d6518647cab2f8b87
SirLavaaa <lava12597@gmail.com>
2026-01-31 14:32:48 +0500

    тест постов

--- Program.cs
+++ Program.cs
@@ -10,18 +10,27 @@ namespace http_practice
     internal class Program
     {
         private static UserService _userService;
+        private static PostsService postsService;
 
         static async Task Main(string[] args)
         {
             _userService = new UserService();
+            _postsService = new PostsService();
 
             var users = await _userService.GetAllAsync();
+            var posts = await _postsService.GetAllAsync();
 
             foreach (var user in users)
             {
                 Console.WriteLine($"ID: {user.Id}");
                 Console.WriteLine($"Username: {user.Username}");
                 Console.WriteLine($"Email: {user.Email}");
+            }
+            foreach(var post in posts)
+            {
+                Console.WriteLine($"ID: {post.Id}");
+                Console.WriteLine($"Title: {post.Title}");
+                Console.WriteLine($"Content: {post.Content}");
             }
         }
     }
