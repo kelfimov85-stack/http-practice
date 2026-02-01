8e1cc6becb0e8c4ad1d0eb721a53974db6eb449d
User <abcd@email.com>
2026-02-01 13:56:46 +0500

    Создал сервис создания поста и добавит CommentDto

---
+++ Dto/CommentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace http_practice.Dto
+{
+    internal class CommentDto
+    {
+        public int Content { get; set; }
+        public string UserId { get; set; }
+        public string PostId { get; set; }
+    }
+}
