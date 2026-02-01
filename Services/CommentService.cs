e590ced4c30d99621164e2c5f27afa8e765edc05
SirLavaaa <lava12597@gmail.com>
2026-02-01 13:56:31 +0500

    сделал сервис для комментов

---
+++ Services/CommentService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace http_practice.Services
+{
+    internal class CommentService
+    {
+        private readonly HttpClient _http;
+        public CommentService()
+        {
+            _http = ApiClient.Instance;
+        }
+        public async Task<HttpResponseMessage> CreateAsync(CommentDto comment)
+        {
+            try
+            {
+                return await _http.PostAsJsonAsync<PostDto>("/api/comments", comment);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error HTTP request: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error get all users: {ex.Message}");
+                throw;
+            }
+        }
+        public async Task<List<CommentDto>> GetAllAsync()
+        {
+            try
+            {
+                return await _http.GetFromJsonAsync<List<PostDto>>("/api/comments");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error HTTP request: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error get all users: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
